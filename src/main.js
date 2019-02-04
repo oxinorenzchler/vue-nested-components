b288a6966241e5247828683783215564b52721cf
oxinorenzchler <oxino.renzchler@gmail.com>
2019-02-04 11:27:08 +0800

    Vue Nested Components

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+/*
+//Import globally
+import Ninjas from './Ninjas.vue'
+Vue.component('ninja',Ninjas);
+*/
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
